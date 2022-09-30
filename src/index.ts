0ffc366cfddd152272f51de071690331c56a5aa1
Léo Genot <lg@mambomambo.ca>
2022-09-30 20:42:48 +0200

    hero using scroll trigger

--- src/index.ts
+++ src/index.ts
@@ -3,115 +3,86 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
-
-/* const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-});
-
-
-scroll.on("scroll", ScrollTrigger.update);
-
-ScrollTrigger.scrollerProxy('[data-scroll-container]', {
-    scrollTop(value) {
-        return arguments.length ? scroll.scrollTo(value, 0, 0) : scroll.scroll.instance.scroll.y;
-    },
-    getBoundingClientRect() {
-        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-    },
-
-    // Apparently, thefollwoing line is not required make pinning work on touch screen (since 4.0.0 ??)
-    // but (since 4.0.0 ??) the grabbing seems buggy, so 3.6.1 is still being used in this demo 
-
-    pinType: document.querySelector('[data-scroll-container]').style.transform ? "transform" : "fixed"
-
-});
-
-ScrollTrigger.defaults({ scroller: '[data-scroll-container]' });
-
-
-
-
-gsap.set('section.footer-container', { yPercent: -50 })
-
-const uncover = gsap.timeline({ paused: true })
-
-uncover
-    .to('section.footer-container', { yPercent: 0, ease: 'none' })
-    ;
-
-ScrollTrigger.create({
-    trigger: 'section.conclusion',
-    start: 'bottom bottom',
-    end: '+=75%',
-    animation: uncover,
-    scrub: true,
-    markers: true,
-})
-
-
-
-
-
-ScrollTrigger.addEventListener("refresh", () => scroll.update());
-
-ScrollTrigger.refresh();  */
-
 const locoScroll = new LocomotiveScroll({
-    el: document.querySelector(".scroller"),
+    el: document.querySelector(".smooth-scroll"),
     smooth: true,
-
-    // // --- // for versions (since 4.0.0 ??)
-    // // --- // for tablet smooth
-    // tablet: { smooth: true },
-    // // --- // for mobile
-    // smartphone: { smooth: true }
-
+    //lerp: 0.08,
 });
+
 locoScroll.on("scroll", ScrollTrigger.update);
 
-ScrollTrigger.scrollerProxy(".scroller", {
+ScrollTrigger.scrollerProxy(".smooth-scroll", {
     scrollTop(value) {
-        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        return arguments.length
+            ? locoScroll.scrollTo(value, 0, 0)
+            : locoScroll.scroll.instance.scroll.y;
     },
     getBoundingClientRect() {
-        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        return {
+            top: 0,
+            left: 0,
+            width: window.innerWidth,
+            height: window.innerHeight,
+        };
     },
-
-    // Apparently, thefollwoing line is not required make pinning work on touch screen (since 4.0.0 ??)
-    // but (since 4.0.0 ??) the grabbing seems buggy, so 3.6.1 is still being used in this demo 
-
-    pinType: document.querySelector(".scroller").style.transform ? "transform" : "fixed"
-
+    pinType: document.querySelector(".smooth-scroll").style.transform
+        ? "transform"
+        : "fixed",
 });
 
-ScrollTrigger.defaults({ scroller: '.scroller' });
+const vw = (coef) => window.innerWidth * (coef / 100);
+const vh = (coef) => window.innerHeight * (coef / 100);
 
+const heroScroller = gsap.timeline({
+    paused: true,
+    scrollTrigger: {
+        trigger: ".hero-header.h-1",
+        scroller: ".smooth-scroll",
+        pin: ".pin-wrapper",
+        start: "top",
+        scrub: true,
+        end: `${vh(100)}`,
+    },
+});
 
-
-
-
-gsap.set('section.footer-container', { yPercent: -50 })
-
-const uncover = gsap.timeline({ paused: true })
-
-uncover
-    .to('section.footer-container', { yPercent: 0, ease: 'none' })
-    ;
-
-ScrollTrigger.create({
-    trigger: 'section.conclusion',
-    start: 'bottom bottom',
-    end: '+=75%',
-    animation: uncover,
-    scrub: true,
-    markers: true,
-})
-
-
-
-
+heroScroller
+    .to(
+        [".hero-header.h-1", ".hero-header.h-3"],
+        {
+            ease: "slow",
+            scale: 2,
+            y: vh(150),
+            xPercent: -150,
+        },
+        "heroScroll"
+    )
+    .to(
+        ".hero-header.h-2",
+        {
+            ease: "slow",
+            scale: 2,
+            y: vh(150),
+            xPercent: 150,
+        },
+        "heroScroll"
+    )
+    .to(
+        "#heroImage",
+        {
+            ease: "slow",
+            scaleY: 2.5,
+        },
+        "heroScroll"
+    )
+    .to(
+        "#heroImage .image",
+        {
+            ease: "slow",
+            scaleX: 2.5,
+            xPercent: 50,
+        },
+        "heroScroll"
+    );
 
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
 ScrollTrigger.refresh();
